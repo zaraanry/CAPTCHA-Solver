9a836e80e0cc15fe542b10fda60441a163dbaa63
Zahra Noury <zaraanry@gmail.com>
2020-01-24 09:35:56 +0330

    Fix Bug

--- Captcha Solver.py
+++ Captcha Solver.py
@@ -219,4 +219,225 @@ for i in range(5):
     acc *= scores[6+i]
 print('Test accuracy: %.2f' % (acc * 100.))
 
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import argparse
+import json
+import string
+import os
+import shutil
+import uuid
+from captcha.image import ImageCaptcha
+
+import itertools
+
+import os
+import cv2
+import numpy as np
+from random import random
+
+import keras
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, Input
+import matplotlib.pyplot as plt
+
+
+def _gen_captcha(img_dir, num_of_letters, num_of_repetition, width, height):
+    if os.path.exists(img_dir):
+        shutil.rmtree(img_dir)
+    if not os.path.exists(img_dir):
+        os.makedirs(img_dir)
+
+    image = ImageCaptcha(width=width, height=height)
+
+    for counter in range(num_of_repetition):
+        print('generating %d/%d' % (counter + 1, num_of_repetition))
+        for i in itertools.permutations([str(c) for c in range(10)], num_of_letters):
+            captcha = ''.join(i)
+            fn = os.path.join(img_dir, '%s_%s.png' % (captcha, uuid.uuid4()))
+            image.write(captcha, fn)
+
+
+def gen_dataset(path, num_of_repetition, num_of_letters, width, height):
+    _gen_captcha(os.path.join(path, 'data'), num_of_letters, num_of_repetition, width, height)
+    print('Finished Data Generation')
+
+
+BATCH_SIZE = 128
+NUM_OF_LETTERS = 5
+EPOCHS = 35
+IMG_ROW, IMG_COLS = 50, 135
+
+
+# Non-configs
+PATH = os.getcwd()
+DATA_PATH = os.path.join(PATH, 'train')
+
+
+def load_data(path, test_split=0.1):
+    print ('loading dataset...')
+    y_train = []
+    y_test = []
+    x_train = []
+    x_test = []
+
+    # r=root, d=directories, f = files
+    counter = 0
+    for r, d, f in os.walk(path):
+        for fl in f:
+            if '.png' in fl:
+                flr = fl.split('_')[0]
+                counter += 1
+                label = np.zeros((NUM_OF_LETTERS, 10))
+                for i in range(NUM_OF_LETTERS):
+                    label[i, int(flr[i])] = 1
+
+                img = cv2.imread(os.path.join(r, fl))
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                img = cv2.resize(img, (int(135/2), int(50/2)), interpolation=cv2.INTER_AREA)
+                img = np.reshape(img, (img.shape[0], img.shape[1], 1))
+
+                if random() < test_split:
+                    y_test.append(label)
+                    x_test.append(img)
+                else:
+                    y_train.append(label)
+                    x_train.append(img)
+
+    print('dataset size:', counter, '(train=%d, test=%d)' % (len(y_train), len(y_test)))
+    return np.array(x_train), np.array(y_train), np.array(x_test), np.array(y_test)
+
+
+if not os.path.exists(DATA_PATH):
+    print('Generating Dataset')
+    gen_dataset(DATA_PATH, 8 , NUM_OF_LETTERS, IMG_COLS, IMG_ROW)
+
+
+x_train, y_train, x_test, y_test = load_data(DATA_PATH)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
+
+
+s_train = []
+s_test = []
+for i in range(NUM_OF_LETTERS):
+    s_train.append(y_train[:, i, :])
+    s_test.append(y_test[:, i, :])
+
+
+save_dir = os.path.join(PATH, 'saved_models')
+model_name = 'keras_cifar10_trained_model.h5'
+
+
+input_layer = Input((25, 67, 1))
+x = Conv2D(filters=32, kernel_size=(5, 5), padding='same', activation='relu')(input_layer)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+x = Conv2D(filters=48, kernel_size=(5, 5), padding='same', activation='relu')(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+x = Conv2D(filters=64, kernel_size=(5, 5), padding='same', activation='relu')(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+x = Dropout(0.3)(x)
+x = Flatten()(x)
+x = Dense(512, activation='relu')(x)
+x = Dropout(0.3)(x)
+
+out = [Dense(10, name='digit%d' % i, activation='softmax')(x) for i in range(NUM_OF_LETTERS)]
+model = Model(inputs=input_layer, outputs=out)
+
+# initiate Adam optimizer
+opt = keras.optimizers.Adam(lr=0.0001, beta_1=0.99, beta_2=0.9999, epsilon=None, decay=0.0, amsgrad=False)
+model.compile(loss='binary_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+
+model.summary()    
+
+
+digit_acc = [[] for _ in range(NUM_OF_LETTERS)]
+val_digit_acc = [[] for _ in range(NUM_OF_LETTERS)]
+loss = []
+val_loss = []
+
+
+def plot_diagram(digit_acc_now, val_digit_acc_now, loss_now, val_loss_now):
+    global digit_acc, val_digit_acc, loss, val_loss
+    for i in range(NUM_OF_LETTERS):
+        digit_acc[i].extend(digit_acc_now[i])
+        val_digit_acc[i].extend(val_digit_acc_now[i])
+    loss.extend(loss_now)
+    val_loss.extend(val_loss_now)
+    
+    
+    for i in range(NUM_OF_LETTERS):
+        plt.plot(digit_acc[i], label='digit%d_acc' % i)
+        plt.plot(val_digit_acc[i], label='val_digit%d_acc' % i)
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend()
+    plt.show()
+
+    # Plot training & validation loss values
+    plt.plot(loss)
+    plt.plot(val_loss)
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+
+history = model.fit(x_train, s_train,
+                    batch_size=BATCH_SIZE,
+                    epochs=EPOCHS,
+                    verbose=1,
+                    validation_data=(x_test, s_test))
+
+
+plot_diagram(
+    [history.history['digit%d_acc' % i] for i in range(NUM_OF_LETTERS)],
+    [history.history['val_digit%d_acc' % i] for i in range(NUM_OF_LETTERS)],
+    history.history['loss'],
+    history.history['val_loss'],
+)
+
+
+# Save model and weights
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+model_path = os.path.join(save_dir, model_name)
+model.save(model_path)
+print('Saved trained model at %s ' % model_path)
+
+
+# Score trained model.
+scores = model.evaluate(x_train, s_train, verbose=1)
+print('Train loss:     %.2f' % np.mean(scores[0:5]))
+acc = 1.
+for i in range(5):
+    acc *= scores[6+i]
+print('Train accuracy: %.2f' % (acc * 100.))
+
+
+scores = model.evaluate(x_test, s_test, verbose=1)
+print('Test loss:     %.2f' % np.mean(scores[0:5]))
+acc = 1.
+for i in range(5):
+    acc *= scores[6+i]
+print('Test accuracy: %.2f' % (acc * 100.))
+
 
